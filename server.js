48e830e38512525b8c95e6cb5b8173c04bfb82bf
Jose Sanchez <jmsanchezdiaz@gmail.com>
2021-02-21 23:22:04 +0000

    JSON new format: { error: 'invalid url' }

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/api/shorturl/new/', bodyParser.urlencoded({extended: false}), (reques
   let UrlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
 
   if(!inputUrl.match(UrlRegex)){
-    response.json({error: 'Invalid URL'})
+    response.json({error: 'invalid url'})
     return
   }
   responseObject['original_url'] = inputUrl
